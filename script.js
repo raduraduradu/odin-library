b5af966f2cd7fd064c075dbe9f2f4eebc0e15664
Radu <radu.stanciu2016@gmail.com>
2024-08-07 21:20:24 +0300

    added functionality for Add Book button so that a modal form for adding books pops up on click added javascript validation for pages field, encoding for <> signs

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ myLibrary.push(new Book("1984", "George Orwell", 328, false));
 function loadBook(id) {
   //let card = document.createElement(div);
   //card.classList.add("book-card");
+
   let book = myLibrary[id];
-  let card =`<div class="book-card">
+  let card =`<div class="book-card" id="card${id}">
   <div>
     <h2>${book.title}</h2>
     <h3>by ${book.author}</h3>
@@ -29,16 +30,8 @@ function loadBook(id) {
 </div>`;
   
   container.insertAdjacentHTML('beforeend', card);
-}
 
-loadBook(0);
-
-function addBookToLibrary() {
-  
-}
-
-document.querySelectorAll(".read-button").forEach((button) =>
-  button.addEventListener("click", (e) => {
+  document.querySelector(`.container #card${id} .read-button`).addEventListener("click", (e) => {
     e.target.classList.toggle("read");
     e.target.classList.toggle("not-read");
     
@@ -49,7 +42,51 @@ document.querySelectorAll(".read-button").forEach((button) =>
       e.target.textContent = "Not read";
     }
 
-    let index = Number(e.target.parentElement.querySelector(".index").textContent.match(/\d+/)[0]); 
-    myLibrary[index].read = !myLibrary[index].read;
+    myLibrary[id].read = !myLibrary[id].read;
   })
-);
+}
+
+loadBook(0);
+
+
+const addBookBtn = document.querySelector("#add-book");
+const dialog = document.querySelector("#modal");
+
+function addBookToLibrary(book) {
+  //encoding < and > characters so html doesn't break
+  book.title = book.title.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  book.author = book.author.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+  myLibrary.push(book);
+  loadBook(myLibrary.length - 1);
+}
+
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+document.querySelector("#modal #form-cancel").addEventListener("click", () => {
+  dialog.close();
+})
+
+let strToBool = (string) => {
+  if(string === "true"){
+    return true;
+  }
+  else if(string === "false"){
+    return false;
+  }
+}
+
+const form = document.querySelector("dialog form");
+form.addEventListener("submit", (e) => {
+  
+  if(isNaN(Number(form["pages"].value))) {
+    e.preventDefault()
+    alert("Input in pages field must be a number");
+  }
+  else {
+    let newBook = new Book(form["book-title"].value, form["author"].value, form["pages"].value, strToBool(form["read"].value));
+    addBookToLibrary(newBook);
+  }
+})
